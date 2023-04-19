070454fffe666fbd31bc183ab03c855fe77f9674
Kure-ru <clairedochez@gmail.com>
2023-04-19 13:48:49 +0900

    refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
+ //pokemon container
+ const pokemonContainer = document.querySelector("#pokemon__container");
+ const pokemonTypeList = document.querySelectorAll("#pokemon-type-list li")
+ const pokemonFilteredByType = document.getElementById("pokemon-filtered-by-type")
+
 //POKEMON NAME SEARCH
 
 //fetch pokemon API from https://pokeapi.co/api/v2/pokemon/
@@ -7,12 +12,14 @@ const searchButton = document.querySelector(".search__button");
 searchButton.addEventListener("click", fetchPokemonData);
 
 async function fetchPokemonData() {
-  const search = document.querySelector("#name").value;
-  const requestPkm = await fetch(`https://pokeapi.co/api/v2/pokemon/${search}`);
+//TODO: error message if wrong input
+const search = document.querySelector("#name").value;
+
+  const requestPkm = await fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`);
   const data = await requestPkm.json();
 
   const requestSpecie = await fetch(
-    `https://pokeapi.co/api/v2/pokemon-species/${search}`
+    `https://pokeapi.co/api/v2/pokemon-species/pikachu`
   );
   const speciesData = await requestSpecie.json();
 
@@ -26,85 +33,17 @@ async function fetchPokemonData() {
     currentEvolution = currentEvolution.evolves_to[0];
     evolutions.push(currentEvolution.species.name);
   }
-
-
   displayPokemonData(data, evolutions);
 }
 
-//SELECTION BY POKEMON TYPES
-const pokemonTypeList = document.querySelectorAll("#pokemon-type-list li")
-const content = document.getElementById("pokemon-filtered-by-type")
-
-//adds event listener to every list in UL element
-for(let i = 0; i < pokemonTypeList.length; i++)
-{
-  pokemonTypeList[i].addEventListener('click', listPokemonsByType)
-}
-
-//if the list of pokemons - filtered by type - exist, remove them
-function clearDOM(){
-  while (content.firstChild) {
-    content.removeChild(content.firstChild);
-  }
-}
-
-//show list of pokemons after you click on a specific
-async function listPokemonsByType(){
-  clearDOM()
-
-  //this.value comes from the value of the LI that you clicked on
-  let url = `https://pokeapi.co/api/v2/type/${this.value}`
-  console.log(this.value)
-
-  fetch(url)
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-      console.log(data.pokemon) //this is array of objects
-
-      //create a HTML div template and lists
-      generateMarkupForPokemonFilteredByType(data);
-    })
-    .catch(err => {
-      console.log(`error ${err}`)
-    });
-}
-
-//function to generate HTML
-function generateMarkupForPokemonFilteredByType(data){
-  let divTemplate = `
-            <div>
-                <h3>${data.name.toUpperCase()} POKEMONS</h3>
-                <ul id="pokemon-list">
-                </ul>
-            </div>
-        ` ;
-  const markup = document.createRange().createContextualFragment(divTemplate).children[0];
-  content.appendChild(markup);
-
-  //select the div that was created on divTemplate
-  const pokemonListUl = document.getElementById('pokemon-list');
-
-  //create an array from the json data
-  const pokemonLi = data.pokemon.map(item => {
-    const listTemplate = `<li><a href="#" data-id="${item.pokemon.url}">${item.pokemon.name} </a></li>`;
-    return document.createRange().createContextualFragment(listTemplate).children[0];
-  });
-
-  pokemonLi.forEach((item, index) => {
-    //create an event listener for every list item, ,
-    item.addEventListener('click', getSelectedPokemonDetails);
-
-    //then append to DOM
-    pokemonListUl.appendChild(item)
-  });
-}
+//////////////////////////////////DUPLICATE
 
 //remove the list and shows the detail of the selected pokemon
 async function getSelectedPokemonDetails(e){
   //console.log(e.currentTarget);
   console.log(e.currentTarget.firstChild.dataset.id)
 
-  //this is duplicate with fetchPokemonData() with some changes
+  //this is duplicate with fetchPokemonData() with some changes!!!!!
   
   const requestPkm = await fetch(e.currentTarget.firstChild.dataset.id);
   const data = await requestPkm.json();
@@ -129,26 +68,96 @@ async function getSelectedPokemonDetails(e){
 
   //the code is a duplicate with fetchPokemonData() with some change... need to edit?
 }
+///////////////////////////////DUPLICATE END
+
+//if the list of pokemons - filtered by type - exist, remove them
+function clearDOM(divToClear){
+  while (divToClear.firstChild) {
+    divToClear.removeChild(divToClear.firstChild);
+  }
+}
+
+//SELECTION BY POKEMON TYPES
+
+
+//adds event listener to every list in UL element
+for(let i = 0; i < pokemonTypeList.length; i++)
+{
+  pokemonTypeList[i].addEventListener('click', listPokemonsByType)
+}
+
+//show list of pokemons after you click on a specific
+async function listPokemonsByType(){
+  clearDOM(pokemonContainer)
+  clearDOM(pokemonFilteredByType)
+
+  //this.value comes from the value of the LI that you clicked on
+  let url = `https://pokeapi.co/api/v2/type/${this.value}`
+  console.log(this.value)
+
+  fetch(url)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data.pokemon) //this is array of objects
+      //create a HTML div template and lists
+      generateMarkupForPokemonFilteredByType(data);
+    })
+    .catch(err => {
+      //display error message 
+      errorMessage.textContent = `${err}`
+    });
+}
+
+//function to generate HTML
+function generateMarkupForPokemonFilteredByType(data){
+  let divTemplate = `
+            <div>
+                <h3>${data.name.toUpperCase()} POKEMONS</h3>
+                <ul id="pokemon-list">
+                </ul>
+            </div>
+        ` ;
+  const markup = document.createRange().createContextualFragment(divTemplate).children[0];
+  pokemonFilteredByType.appendChild(markup);
+
+  //select the div that was created on divTemplate
+  const pokemonListUl = document.getElementById('pokemon-list');
+
+  //create an array from the json data
+  const pokemonLi = data.pokemon.map(item => {
+    const listTemplate = `<li><a href="#" data-id="${item.pokemon.url}">${item.pokemon.name} </a></li>`;
+    return document.createRange().createContextualFragment(listTemplate).children[0];
+  });
+
+  pokemonLi.forEach((item, index) => {
+    //create an event listener for every list item, ,
+    item.addEventListener('click', getSelectedPokemonDetails);
+
+    //then append to DOM
+    pokemonListUl.appendChild(item)
+  });
+}
+
+
 
     //single pokemon display
 function displayPokemonData(data, evolutions) {
   console.log(data);
-  //pokemon container
-  let pokemonContainer = document.querySelector("#pokemon__container");
+ 
   //name
-  let pokemonName = document.createElement("h1");
-  pokemonName.textContent = data.name;
+  // let pokemonName = document.createElement("h1");
+  // pokemonName.textContent = data.name;
   //id
-  let pokemonId = document.createElement("p");
-  pokemonId.textContent = `#${data.id}`;
+  // let pokemonId = document.createElement("p");
+  // pokemonId.textContent = `#${data.id}`;
 
   //img
-  let pokemonPic = document.createElement("img");
-  pokemonPic.src = data.sprites.front_default;
+  // let pokemonPic = document.createElement("img");
+  // pokemonPic.src = data.sprites.front_default;
 
   //type
-  let pokemonType = document.createElement("p");
-  pokemonType.textContent = data.types[0].type.name;
+  // let pokemonType = document.createElement("p");
+  // pokemonType.textContent = data.types[0].type.name;
 
   //stats
   let statsList = document.createElement("ul");
@@ -158,12 +167,12 @@ function displayPokemonData(data, evolutions) {
     statsList.appendChild(statItem);
   });
   //stats
-  let abilitiesList = document.createElement("ul");
-  data.abilities.forEach((ability) => {
-    let abilityItem = document.createElement("li");
-    abilityItem.textContent = ability.ability.name;
-    abilitiesList.appendChild(abilityItem);
-  });
+  // let abilitiesList = document.createElement("ul");
+  // data.abilities.forEach((ability) => {
+  //   let abilityItem = document.createElement("li");
+  //   abilityItem.textContent = ability.ability.name;
+  //   abilitiesList.appendChild(abilityItem);
+  // });
 
   //evolutions
   const evolutionContainer = document.createElement("div");
@@ -184,16 +193,47 @@ function displayPokemonData(data, evolutions) {
     evolutionContainer.appendChild(pokemonImage);
   });
 
-  // Clear previous search results
-  pokemonContainer.innerHTML = "";
+  let pokemonTemplate = `
+  <div>
+    <div class="left-container">
+      <img src="">
+      <h2>${data.name}</h2>
+      <span>#${data.id}</span>
+      <span>${data.types[0].type.name}</span>
+    </div>
+
+    <div class="right-container">
+      <ul> <li>Test</li>
+      </ul>
+    </div>
+
+    <div>
+      <p></p>
+    </div>
+  </div>
+  `
+  const markup = document.createRange().createContextualFragment(pokemonTemplate).children[0];
+  pokemonContainer.appendChild(markup);
+
+  /*
+  let divTemplate = `
+            <div>
+                <h3>${data.name.toUpperCase()} POKEMONS</h3>
+                <ul id="pokemon-list">
+                </ul>
+            </div>
+        ` ;
+  const markup = document.createRange().createContextualFragment(divTemplate).children[0];
+  pokemonFilteredByType.appendChild(markup);
+  */
   //append to container
-  pokemonContainer.appendChild(pokemonName);
-  pokemonContainer.appendChild(pokemonId);
-  pokemonContainer.appendChild(pokemonPic);
-  pokemonContainer.appendChild(pokemonType);
-  pokemonContainer.appendChild(abilitiesList);
-  pokemonContainer.appendChild(statsList);
-  pokemonContainer.appendChild(evolutionContainer);
+  // pokemonContainer.appendChild(pokemonName);
+  // pokemonContainer.appendChild(pokemonId);
+  // pokemonContainer.appendChild(pokemonPic);
+  // pokemonContainer.appendChild(pokemonType);
+  // pokemonContainer.appendChild(abilitiesList);
+  // pokemonContainer.appendChild(statsList);
+  // pokemonContainer.appendChild(evolutionContainer);
 }
 
 //get and store data from input form
